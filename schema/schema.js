6a5647bbcfa59f4ca1578f84f72dc16e7190d2ee
mickeymond <mickeymond@gmail.com>
2019-04-14 16:27:22 +0000

    Initial Commit

---
+++ schema/schema.js
@@ -0,0 +1,41 @@
+const graphql = require('graphql');
+const _ = require('lodash');
+const {
+    GraphQLSchema,
+    GraphQLObjectType,
+    GraphQLString,
+    GraphQLInt
+} = graphql;
+
+const users = [
+    { id: '23', firstName: 'Michael', age: 23 },
+    { id: '45', firstName: 'Samuel', age: 20 },
+    { id: '67', firstName: 'Oliver', age: 21 },
+    { id: '12', firstName: 'Janet', age: 32 },
+];
+
+const UserType = new GraphQLObjectType({
+    name: 'User',
+    fields: {
+        id: { type: GraphQLString },
+        firstName: { type: GraphQLString },
+        age: { type: GraphQLInt }
+    }
+});
+
+const RootQuery = new GraphQLObjectType({
+    name: 'RootQueryType',
+    fields: {
+        user: {
+            type: UserType,
+            args: { id: { type: GraphQLString } },
+            resolve(parentValue, args) {
+                return _.find(users, { id: args.id });
+            }
+        }
+    }
+});
+
+module.exports = new GraphQLSchema({
+    query: RootQuery
+});
